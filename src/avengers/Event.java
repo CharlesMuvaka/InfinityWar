e8c85e7ca2d79a708df0eea5af41dc7a560e7ef5
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-12-22 02:24:25 +0300

    generate getters and setters of the class.

--- src/avengers/Event.java
+++ src/avengers/Event.java
@@ -13,5 +13,27 @@ public class Event {
 //        this.weight = weight;
     }
 
+    public int getIndex() {
+        return index;
+    }
 
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public List<Event> getNeighbors() {
+        return neighbors;
+    }
+
+    public void setNeighbors(Event neighbors) {
+        this.neighbors.add(neighbors);
+    }
 }
