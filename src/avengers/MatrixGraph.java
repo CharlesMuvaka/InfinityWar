3640b55f15801d67e6803d50790d49842908ea08
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-12-22 01:27:46 +0300

    add member variables of the matrix graph

---
+++ src/avengers/MatrixGraph.java
@@ -0,0 +1,12 @@
+package avengers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MatrixGraph {
+    private int[][] adjacencyMatrix;
+    private int vertices;
+    private Map<Integer,Double> verticesCost;
+
+
+}
