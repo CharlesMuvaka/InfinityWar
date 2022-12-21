df7fdc9e7930837ef6cf8da7554ec49766f40af9
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-12-22 02:23:23 +0300

    create member variables of the event class

---
+++ src/avengers/Event.java
@@ -0,0 +1,12 @@
+package avengers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Event {
+    private int index;
+    private int weight;
+    private List<Event> neighbors = new ArrayList<>();
+
+
+}
