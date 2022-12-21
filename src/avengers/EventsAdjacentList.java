e7fad8c8d62c263a71e546e9eabf7a2a9391a56f
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-12-22 02:19:22 +0300

    create a constructor to be used in initialising the adjacency list.

--- src/avengers/EventsAdjacentList.java
+++ src/avengers/EventsAdjacentList.java
@@ -6,5 +6,9 @@ public class EventsAdjacentList {
     private Event[] events;
     private int noOfEvents;
 
+    public EventsAdjacentList(int noOfEvents) {
+        this.noOfEvents = noOfEvents;
+        this.events = new Event[noOfEvents];
+    }
 
 }
