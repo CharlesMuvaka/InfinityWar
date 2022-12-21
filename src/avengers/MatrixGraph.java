820387c6e19c6b9c450d9dc017c2912b5ae9a70d
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-12-22 01:28:38 +0300

    create overloaded constructors to be used in the matrix graph creation

--- src/avengers/MatrixGraph.java
+++ src/avengers/MatrixGraph.java
@@ -8,5 +8,15 @@ public class MatrixGraph {
     private int vertices;
     private Map<Integer,Double> verticesCost;
 
+    public MatrixGraph(int vertices){
+        this.vertices = vertices;
+    }
+
+    public MatrixGraph(int no_of_vertices, Map<Integer,Double> verticesCost){
+        this.verticesCost = verticesCost;
+        this.vertices = no_of_vertices;
+        this.adjacencyMatrix = new int[no_of_vertices][no_of_vertices];
+    }
+
 
 }
